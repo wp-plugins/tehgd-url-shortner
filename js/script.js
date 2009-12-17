743d37bcc4f90589b919988f59fecb2cad5deb83
sam_benne <sam_benne@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-17 19:07:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tehgd-url-shortner/tags/0.3@184009 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/script.js
@@ -0,0 +1,17 @@
+﻿jQuery(function() {
+	jQuery('#submit-tehgd').click(function() {
+		tehgd(jQuery('#long-tehgd').val(),'#short-tehgd');
+	});
+	jQuery('#long-tehgd').blur(function() {
+		jquery(this).val('');
+	});
+	
+	jQuery('#short-tehgd').click(function() {
+		this.select();
+	});
+	function tehgd(link,place) {
+		jQuery.getJSON("http://teh.gd/json.php?u=" + link + "&callback=?", function(data){
+				jQuery(place).text(data.url);
+		});
+	}
+});
